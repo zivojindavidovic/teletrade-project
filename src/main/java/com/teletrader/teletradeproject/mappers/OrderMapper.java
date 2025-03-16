d801ec37bd9151bac023fcf4cfe6bd0cde935433
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-03-16 15:31:38 +0100

    Changed order mapper

--- src/main/java/com/teletrader/teletradeproject/mappers/OrderMapper.java
+++ src/main/java/com/teletrader/teletradeproject/mappers/OrderMapper.java
@@ -1,7 +1,11 @@
 package com.teletrader.teletradeproject.mappers;
 
+import com.teletrader.teletradeproject.controllers.v1.order.response.OrderDTO;
 import com.teletrader.teletradeproject.controllers.v1.order.response.OrderSaveResultDTO;
+import com.teletrader.teletradeproject.controllers.v1.stock.response.StockOrderBookDTO;
 import com.teletrader.teletradeproject.models.Order;
+
+import java.util.List;
 
 public class OrderMapper {
 
@@ -11,6 +15,24 @@ public class OrderMapper {
                 .price(order.getPrice())
                 .quantity(order.getQuantity())
                 .type(order.getType().toString())
+                .build();
+    }
+
+    public static List<OrderDTO> toOrdersResponse(List<Order> orders) {
+        return orders.stream()
+                .map(order -> OrderDTO.builder()
+                        .orderId(order.getOrderId())
+                        .price(order.getPrice())
+                        .quantity(order.getQuantity())
+                        .type(order.getType().toString())
+                        .build())
+                .toList();
+    }
+
+    public static StockOrderBookDTO toOrderBookResponse(Long stockId, List<Order> orders) {
+        return StockOrderBookDTO.builder()
+                .stockId(stockId)
+                .orders(toOrdersResponse(orders))
                 .build();
     }
 }
